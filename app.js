924e5d87218c0cd38f9430c82cd10ff298adccec
root <root@ip-172-31-36-154.ap-south-1.compute.internal>
2026-05-22 04:22:49 +0000

    deployment test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.write('Application Running');
+  res.write('Application Running sucessfully');
   res.end();
 }).listen(3000, '0.0.0.0');
 
